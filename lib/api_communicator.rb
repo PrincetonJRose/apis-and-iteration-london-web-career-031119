474f751b0e6cf191d647399d2983a023616bae9d
PrincetonJRose <decoyzpr@gmail.com>
2019-03-12 15:21:47 -0400

    Displaying Movie Titles Yay

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,7 +6,8 @@ def get_character_movies_from_api(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-
+  response_films = RestClient.get('http://www.swapi.co/api/films/')
+  films_hash = JSON.parse(response_films)
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -16,10 +17,28 @@ def get_character_movies_from_api(character_name)
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
+  films_in = {}
+  response_hash['results'].each do |result|
+    if result['name'].downcase == character_name
+      for number in 0...result['films'].size
+        film_title = result['films'][number]
+        get_title = RestClient.get(film_title)
+        get_title = JSON.parse(get_title)
+        title = get_title['title']
+        films_in[result['films'][number]] = title
+      end
+    end
+  end
+  return films_in
 end
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
+  # films_in.each do |film|
+  # puts film['title']
+  films.each do |film, title|
+    puts title
+  end
 end
 
 def show_character_movies(character)
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,10 @@
 def welcome
   # puts out a welcome message here!
+  puts "Hi there! Get your Star Wars information here!"
 end
 
 def get_character_from_user
-  puts "please enter a character name"
+  puts "Enter the name of the character you would like to know about:"
   # use gets to capture the user's input. This method should return that input, downcased.
+  gets.chomp.downcase
 end
